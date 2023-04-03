fa8209c12091b695de7eba8980ba605cf91e98c5
unknown <harriett.steed@gmail.com>
2023-04-03 02:25:48 -0600

    weather cards that i probs wont need

--- Develop/script.js
+++ Develop/script.js
@@ -7,20 +7,26 @@ var requestOptions = {
   };
 var city = "farmington";
   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=98cc20ce5aa0b82e2cd1d17c1146fc28`, requestOptions)
-  .then(response => response.json())
-  .then(result => {
+        .then(response => response.json())
+        .then(result => {
     // todo 
     var lat = 44;
     var lon = 31;
+
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=98cc20ce5aa0b82e2cd1d17c1146fc28`, requestOptions)
         .then(response => response.json())
         .then(result => {
+          
 // todo
     console.log(result)
         })
         .catch(error => console.log('error', error));
     })
   .catch(error => console.log('error', error));
+
+
+
+  
   
 
 
